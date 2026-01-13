3773da960252d75737c8594abea6982472ea65be
mrashadyusuf <mrashadyusuf@gmail.com>
2026-01-14 03:27:37 +0700

    add comment on controller

--- src/main/java/com/bezkoder/springjwt/controllers/FinanceController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/FinanceController.java
@@ -18,13 +18,22 @@ public class FinanceController {
     private final Map<String, IDRDataFetcherStrategy> strategyMap;
 
     public FinanceController(StrategyRegistry registry) {
-        this.strategyMap = registry.strategyMap();
+        this.strategyMap = registry.strategyMap(); //mengam bil nama resourceType dan  incstance classdari masing" strategy
     }
 
     @GetMapping("/data/{resourceType}")
-    public List<Object> getData(@PathVariable String resourceType) {
+    public List<Object> getData(@PathVariable String resourceType) {//resourceType = latest_idr_rates
+        // Ambil strategy berdasarkan resourceType, jalankan method getData(), jika strategy tidak ditemukan → lempar exception khusus.
         return Optional.ofNullable(strategyMap.get(resourceType))
                 .map(IDRDataFetcherStrategy::getData)
-                .orElseThrow(() -> new ResourceTypeNotFoundException(resourceType));
+                .orElseThrow(() -> new ResourceTypeNotFoundException(resourceType)); 
     }
+    // HistoricalIdrUsdStrategy, LatestIdrRatesStrategy, dan SupportedCurrenciesStrategy mengimplementasikan IDRDataFetcherStrategy
+
+    //this.strategyMap: {
+    //     historical_idr_usd = com.bezkoder.springjwt.strategy.HistoricalIdrUsdStrategy@4e642ee1,
+    //     supported_currencies = com.bezkoder.springjwt.strategy.SupportedCurrenciesStrategy@2fd954f,
+    //     latest_idr_rates = com.bezkoder.springjwt.strategy.LatestIdrRatesStrategy@29ebbdf4
+    //   }
+      
 }
